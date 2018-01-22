7cb2aef5c52fcfd49ffbceae1b7b7a881b9466dd
seth <furman.seth@gmail.com>
2018-01-22 09:58:54 -0500

    remove nav bar javascript

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -3,29 +3,6 @@ window.onload = function () {
     // hide holiday hours by default
     document.getElementById("holiday-hours").style.display = "none";
     
-    mainNav = Array.from(document.getElementById("main-nav").getElementsByTagName("ul")[0].getElementsByTagName("li"));
-    
-    mainNav.forEach(function (navBtn) {
-        link = navBtn.getElementsByTagName("a")[0];
-        link.onclick = function () {
-            
-            // TODO: why do I need to reassign link?
-            
-            link = navBtn.getElementsByTagName("a")[0];
-            link.setAttribute("class", "active");
-            mainNav.filter(elt => elt.id != navBtn.id).forEach(function (otherBtn) {
-                otherBtn.getElementsByTagName("a")[0].removeAttribute("class");
-                document.getElementById("content-" + otherBtn.className).style.display = "none";
-            });
-            
-            document.getElementById("content-" + navBtn.className).style.display = "";
-            
-            // Don't scroll to ...#content-... element
-            // Credit: https://stackoverflow.com/questions/1255726/href-dont-scroll
-//            return false;
-        }
-    });
-    
     // TODO: load hours data
     
     // TODO: refactor into function addRowToTable or similar...
